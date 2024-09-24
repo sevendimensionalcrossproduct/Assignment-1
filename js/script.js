0dc8ac1279b7aaf9867ac6e128d5a74367b05791
sevendimensionalcrossproduct <daim170743@gmail.com>
2024-09-24 14:07:17 -0700

    script.js

---
+++ js/script.js
@@ -0,0 +1,53 @@
+//STEP 1
+/*
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+*/
+//STEP 2
+let age = 25; //Numeric literal expression (integer)
+let address = "10 Downing St." //String literal expression
+let isThisComputerOn = true; //Boolean literal expression
+let whatHappensAfterDeath = null //Null literal expression
+//STEP 3
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
+
+//STEP 18
+
+//STEP 19
+
+//STEP 20
+
+//STEP 21
+
+//STEP 22
+
